4577af67c310a57971028581ca19f33402e57e52
Prisca <priscamasereli@gmail.com>
2025-09-01 13:57:03 +0200

    Feat: Add the todo list

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { useTodos } from "../context/TodoContext";
+import TodoItem from "./TodoItem";
 
 const TodoList = () => {
-  return <div></div>;
+  const { todos } = useTodos();
+
+  return (
+    <table className="w-full border-collapse border">
+      <thead>
+        <tr className="bg-gray-200">
+          <th className="px-4 py-2 text-left">Task</th>
+          <th className="px-4 py-2 text-left">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {todos.length > 0 ? (
+          todos.map((todo) => <TodoItem key={todo.id} todo={todo} />)
+        ) : (
+          <tr>
+            <td colSpan="2" className="text-center py-4 text-gray-500">
+              No tasks available
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
 };
 
 export default TodoList;
