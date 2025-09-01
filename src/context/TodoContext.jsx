cd4b9cea68020547c9b5a35bdd4684551c0a0c1b
Prisca <priscamasereli@gmail.com>
2025-09-01 13:02:22 +0200

    Feat: Add the todo reducer

--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { createContext } from "react";
 
-const TodoContext = () => {
-  return <div></div>;
-};
+const TodoContext = createContext();
+const initialState = [];
+
+function todoReducer(state, action) {
+  switch (action.type) {
+    case "ADD":
+      return [
+        ...state,
+        { id: Date.now(), text: action.payload, completed: false },
+      ];
+    case "EDIT":
+      return state.map((todo) =>
+        todo.id === action.payload.id
+          ? { ...todo, text: action.payload.text }
+          : todo
+      );
+    case "TOGGLE":
+      return state.map((todo) =>
+        todo.id === action.payload
+          ? { ...todo, completed: !todo.completed }
+          : todo
+      );
+    case "REMOVE":
+      return state.filter(todo.id !== action.payload);
+    default:
+      return state;
+  }
+}
 
 export default TodoContext;
