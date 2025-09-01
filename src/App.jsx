4917b95eb22f2ad792497e5fcd40b9843458a28b
Prisca <priscamasereli@gmail.com>
2025-09-01 13:59:16 +0200

    Feat: Add the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import TodoForm from "./Components/TodoForm";
+import TodoList from "./Components/TodoList";
+import { TodoProvider } from "./context/TodoContext";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <h1 className="text-2xl  text-center font-bold">ContectAPI todo list</h1>
-    </>
+    <TodoProvider>
+      <div className="max-w-2xl mx-auto mt-10 p-6 bg-white rounded shadow">
+        <h1 className="text-2xl font-bold mb-4 text-center">
+          ToDo App (Context API)
+        </h1>
+        <TodoForm />
+        <TodoList />
+      </div>
+    </TodoProvider>
   );
-}
+};
 
 export default App;
